41b20bc8e55e6699d1ff2c5e73a9c04f1029a9b1
Rice_Field <famima65536@gmail.com>
2017-10-22 23:24:30 +0900

    add Editor + Range

---
+++ src/famima65536/PocketBuilder/utils/Editor.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace famima65536\PocketBuilder\utils;
+
+
+use pocketmine\Player;
+
+use famima65536\PocketBuilder\other\Range;
+
+class Editor {
+
+  public static function showRange(Player $player, Range $range) {
+
+  }
+
+}
