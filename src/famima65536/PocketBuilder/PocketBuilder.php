3c0e58225dcd65b5db91e2321be0e2faa7e88465
Rice_Field <famima65536@gmail.com>
2017-10-22 22:03:45 +0900

    add base

---
+++ src/famima65536/PocketBuilder/PocketBuilder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace famima65536\PocketBuilder;
+
+# Base #
+use pocketmine\plugin\PluginBase;
+
+class PocketBuilder extends PluginBase {
+
+  public function onLoad() {
+  }
+
+  public function onEnable() {
+  }
+
+  public function onDisable() {
+  }
+  
+}
