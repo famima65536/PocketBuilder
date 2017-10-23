270027e4033d9fea0c02d800a45ebede68fbb16d
Rice_Field <famima65536@gmail.com>
2017-10-23 12:55:16 +0900

    range!

--- src/famima65536/PocketBuilder/other/Range.php
+++ src/famima65536/PocketBuilder/other/Range.php
@@ -2,6 +2,28 @@
 
 namespace famima65536\PocketBuilder\other;
 
+use pocketmine\level\Position;
+
 class Range {
-  
+
+  public $position = []; /** @var Position[] */
+
+  public function add(Position $pos) : void {
+    $this->position[] = $pos;
+  }
+
+  public function calculateRangeSize() : ?int {
+    if(count($this->position) < 2) {
+      return null;
+    }
+
+    $posList = $this->position;
+
+    return abs(
+       ($posList[0]->x - $posList[1]->x)
+     * ($posList[0]->y - $posList[1]->y)
+     * ($posList[0]->z - $posList[1]->z)
+    );
+  }
+
 }
