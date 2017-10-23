f942a18e00e23a73fbcd22fef578fa0f0d14228d
Rice_Field <famima65536@gmail.com>
2017-10-23 12:55:04 +0900

    update Editor

--- src/famima65536/PocketBuilder/utils/Editor.php
+++ src/famima65536/PocketBuilder/utils/Editor.php
@@ -9,8 +9,16 @@ use famima65536\PocketBuilder\other\Range;
 
 class Editor {
 
-  public static function showRange(Player $player, Range $range) {
+  public static function showRange(Player $player, ?Range $range = null) : bool {
+    $range = $range ?? RangeManager::getRange($player) ?? null;
 
+    if($range === null) {
+      return false;
+    }
+    
+    // TODO run ShowRangetask.
+
+    return true;
   }
 
 }
